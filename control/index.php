72eb810b0966f038f1ae71762829103a9899fab1
Arvid Dezittere <43497237+DraxAris@users.noreply.github.com>
2019-02-22 13:27:06 +0100

    Create index.php

---
+++ control/index.php
@@ -0,0 +1,16 @@
+<?php
+  echo "control module...";
+  get[https://programmingfish.net/icework/controlmod];
+  get[https://google.com/dev/control];
+  get[https://github.com/programmingfish/redirecthub/php/files.php]
+  if(connection = false){
+    echo "connection error";
+    }else{
+      echo "checking files";
+      if(file_get = false){
+        echo "file/ resource not found";
+      }else{
+        echo "all files loaded succesfull";
+        }
+    }
+?>
