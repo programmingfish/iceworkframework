147b66c1855945a98834a579a8a37329cee2ea6b
Arvid Dezittere <43497237+DraxAris@users.noreply.github.com>
2019-02-23 15:24:09 +0100

    Delete files.js

--- js/files.js
+++
@@ -1,6 +0,0 @@
-//this file gets all the css files
-//just upload this file to your servers
-window.onload = files();
-function files(){
-  document.write('<script type="text/javascript" src="https://github.com/programmingfish/iceworkframework/js/link.JSON"></script>');
-}
