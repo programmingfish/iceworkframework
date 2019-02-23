b3d61a5272dedc7f1217c7b5dec8a8172ec70eeb
Arvid Dezittere <43497237+DraxAris@users.noreply.github.com>
2019-02-23 15:15:20 +0100

    Update files.js

--- js/files.js
+++ js/files.js
@@ -2,7 +2,5 @@
 //just upload this file to your servers
 window.onload = files();
 function files(){
-  readTextFile("https://github.com/programmingfish/iceworkframework/js/link.JSON", function(text){
-  var data = JSON.parse(text);
-  console.log(data);
+  document.write('<script type="text/javascript" src="https://github.com/programmingfish/iceworkframework/js/link.JSON"></script>');
 }
